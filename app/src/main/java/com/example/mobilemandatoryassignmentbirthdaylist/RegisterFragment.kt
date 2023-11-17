eb8da27d01d89e978bae465f36c16f952880acd9
slitherman <ericcartman2700@gmail.com>
2023-11-17 03:14:01 +0100

    added auth but the app crashes if you log in after youve logged out

--- app/src/main/java/com/example/mobilemandatoryassignmentbirthdaylist/RegisterFragment.kt
+++ app/src/main/java/com/example/mobilemandatoryassignmentbirthdaylist/RegisterFragment.kt
@@ -47,7 +47,7 @@ class RegisterFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-     _binding = FragmentRegisterBinding.inflate(inflater, container, false)
+        _binding = FragmentRegisterBinding.inflate(inflater, container, false)
         return binding.root
     }
 
